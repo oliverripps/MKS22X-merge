0141bb1743234a6123d58abfa7984bed305b4d49
Oliver Ripps <oripps@stuy.edu>
2019-03-15 10:54:57 -0400

    added psudocode

--- Merge.java
+++ Merge.java
@@ -1,5 +1,17 @@
 public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+    /*Pseudocode:
+    mergesort(data,lo,hi):
+    if lo >= hi :
+      return
+    mergesort left side
+    mergesort right side
+    merge*/
 
 
-  
+  }
+
+
+
 }

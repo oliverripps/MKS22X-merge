87c79604979a253c589f36f4123312338dbbcbff
Oliver Ripps <oripps@stuy.edu>
2019-03-19 08:56:38 -0400

    added call to helper and split part

--- Merge.java
+++ Merge.java
@@ -1,25 +1,21 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
-
-
+    mergesort(data, 0, data.length);
   }
   public static void mergesort(int[] data, int low, int hi){
-    /*Pseudocode:
-    mergesort(data,lo,hi):
-    if lo >= hi :
-      return
-    mergesort left side
-    mergesort right side
-    merge*/
-    //RECREATING Pseudocode
-    int middle=(hi-low)/2;
-    if(low>=hi){
-      return;
-    }
-    mergesort(data,0,middle);
-    mergesort(data,middle+1,data.length-1);
-    
+    int check=low+1;
+    if(check>= hi) {
+       return;
+     }
+    //Creating subarrays
+     int[] lohalf = Arrays.copyOfRange(data, lo, (hi - lo)/2);
+     int[] hihalf = Arrays.copyOfRange(data,(hi - lo)/2, hi);
+     //mergesort on each sub array
+     mergesort(lohalf, 0, lohalf.length);
+     mergesort(hihalf, 0, hihalf.length);
+
+
   }
 
 

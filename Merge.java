f604eb84364631ece03bbc1f5a08a2c69e71db7b
Oliver Ripps <oripps@stuy.edu>
2019-03-20 08:47:21 -0400

    added in current int array

--- Merge.java
+++ Merge.java
@@ -17,11 +17,16 @@ public class Merge{
 
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data){
+    int[] curr = new int[data.length];
+    for(int i=0;i<data.length; x++){
+      curr[i]=data[i];
+    }
+
     if(data.length<x){
       insertionsort(data);
     }
     else{
-    mergesort(data, 0, data.length);
+    mergesort(data,curr, 0, data.length);
   }
   }
   public static void mergesort(int[] data, int lo, int hi){
